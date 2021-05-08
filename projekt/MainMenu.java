384c909b3d59881ff3c8765a969c8d204f906791
Michał Grzegorz Bojke <m.bojke@stud.elka.pw.edu.pl>
2021-05-08 11:37:46 +0200

    Working on Main Menu frame (added buttons and layout manager)

--- projekt/MainMenu.java
+++ projekt/MainMenu.java
@@ -1,14 +1,26 @@
 package projekt;
 
 import java.awt.*; 
+import java.awt.event.*; 
 
 class MainMenu extends Frame {
-void displayMenu(){
-Button b=new Button("LOL");  
-b.setBounds(100,100,80,30);
-add(b);
-setSize(400,400);  
-setLayout(null);
-setVisible(true);
-}
+
+    void displayMenu(){
+
+        Button startGame=new Button("Start Game");  
+        Button options=new Button("Options");
+
+        add(startGame, BorderLayout.CENTER);
+
+        add(options, BorderLayout.NORTH);
+
+        setSize(500,500);  
+        setVisible(true);
+        this.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent we) {
+                dispose();
+                }
+            }
+        );
+    }
 }
